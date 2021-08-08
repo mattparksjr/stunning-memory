5cd47f7139d535e21ee800dd13bb4970e0960290
Matthew Parks <mattparksjr@gmail.com>
2021-08-08 15:41:12 -0400

    Github integration

---
+++ src/FPSCounter.gd
@@ -0,0 +1,10 @@
+## Simple FPS counter
+## Author: Matthew
+
+extends Label
+
+func _ready():
+	set_process(true)
+	
+func _process(delta):
+	self.text = 'FPS: ' + str(Engine.get_frames_per_second())

0f4dce0874d5f63e99925baece067b5cb5513a44
Matthew Parks <mattparksjr@gmail.com>
2021-08-16 03:34:13 -0400

    Title

--- game/src/SceneHandler.gd
+++ game/src/SceneHandler.gd
@@ -1,8 +1,12 @@
 extends Node
 
 var map_start = preload("res://worlds/World.tscn")
+var title_screen = preload("res://ui/TitleScreen.tscn")
 
 func _ready():
-	print("Loading map")
-	var mapstart_ins = map_start.instance()
-	add_child(mapstart_ins)
+	print("Loading UI")
+	var title_screen_ins = title_screen.instance()
+	add_child(title_screen_ins)
+	#print("Loading map")
+	#var mapstart_ins = map_start.instance()
+	#add_child(mapstart_ins)
